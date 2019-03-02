b0cb43cf8563ebfcba5acec0b2bfcd75a952c518
Patrycja <patrycja.cieplicka@gmail.com>
2019-03-02 14:19:33 +0100

    version 1.1 create classes

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fruit{
+    private:
+        string name;
+
+    public:
+
+};
+
+class Box {
+    private:
+        string color;
+        Fruit leftTop;
+        Fruit rightTop;
+        Fruit leftBottom;
+        Fruit rightBottom;
+
+
+    public:
+};
+
+int main(){
+
+cout << "hello world" << endl;
+
+return 0;
+}
 
