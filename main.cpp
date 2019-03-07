22797613a52a9522d78b131adb8e569555bb4100
Patrycja <patrycja.cieplicka@gmail.com>
2019-03-07 12:56:55 +0100

    final dbg+valgrind

--- main.cpp
+++ main.cpp
@@ -16,16 +16,18 @@ int main(){
     Box firstBox;
     Box secondBox;
 
-
+    //bledy (valgrind+dbg)
     int *boxes = new int[2];
     Box *boxestwo = new Box[3];
 
     int x = 0;
     delete [] boxes;
-    boxes[1] = 4.3;
-    int number = 2.4;
-    int number_two = 2.4/x;
+    boxes[1] = 4.3; // po uwolnieniu pamieci
+    int number = 2.4; // zamiana zmiennoprzecinkowej na int
+    int number_two = 2.4/x; //dzielenie przez 0
     cout << number_two << endl;
+
+    //ropzoczecie programu
 
     int process = 1;
     int action;
@@ -33,29 +35,30 @@ int main(){
         printStart();
         cin >> action;
         switch (action){
-            case 1:
+            case 1: //wypisz skrzynke
                 if(whichBox() == 1)
                     printBoxes(firstBox);
                 else
                     printBoxes(secondBox);
                 break;
-            case 2:
-                if(whichBox() == 1)
+            case 2: //dodaj figure do pudelka
+                if(whichBox() == 1) // do skrzynki 1
                     firstBox.addFigure(whichBoxPart(), whichName(), whichColor());
-                else
+                else //do skrzynki 2
                     secondBox.addFigure(whichBoxPart(), whichName(), whichColor());
                 break;
-            case 3:
-                if(whichBox() == 1)
+            case 3: //wyjmij figure z pudelka
+                if(whichBox() == 1) // z skrzynki 1
                     firstBox.deleteFigure(whichBoxPart());
-                else
+                else //z skrzynki 2
                     secondBox.deleteFigure(whichBoxPart());
                 break;
-            case 4:
+            case 4: //wyjdz z programu
                 process = 0;
                 break;
             default:
                 cout << "Zly numer. Wpisz ponownie" << endl;
+                // brak break
         }
     }
 
